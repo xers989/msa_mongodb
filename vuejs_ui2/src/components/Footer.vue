10c7044cc109099b96a984c6139d3a1a9f88a0fb
Xers9 <xers989@gmail.com>
2021-12-17 12:19:21 +0900

    Delete vuejs_ui2 directory

--- vuejs_ui2/src/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-footer height="auto" class="font-weight-medium">
-    <v-card-text class="py-2 text-center">
-      {{ new Date().getFullYear() }} —
-      <strong>MongoDB MSA</strong>
-    </v-card-text>
-  </v-footer>
-</template>
